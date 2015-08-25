ff75f10115eab011b0ed80adf2c643282736e81c
mateusz.holubowski <mateusz.holubowski@thbsolutions.com>
2015-08-25 22:50:11 +0200

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Mateusz Holubowski on 25/08/15.
+//  Copyright (c) 2015 Happy API. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
